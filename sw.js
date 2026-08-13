124cc3ae38f4eb253766a673093fbb152bc7ad10
elektrokrieg <c.berthold@elektrokrieg-meiningen.de>
2026-08-13 08:55:05 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker – Elektro Krieg Regiebericht
 // Версия кэша – увеличивайте при обновлении файлов
-const CACHE_NAME = 'regiebericht-v47';
+const CACHE_NAME = 'regiebericht-v48';
 
 const ASSETS = [
   'https://elektrokrieg.github.io/regiebericht/',
